b80722c3aad68d8d730e31d4c1bbb4b21c2ed4fa
Toxich-UA <ventel911@yandex.ru>
2016-03-19 08:47:13 +0200

    Edit resize method

---
+++ FormsTest/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace FormsTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panel1.Location = new System.Drawing.Point(196, 88);
+            this.panel1.MaximumSize = new System.Drawing.Size(500, 500);
+            this.panel1.MinimumSize = new System.Drawing.Size(100, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(178, 160);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 345);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FormsTest/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool isDown;
+        bool isPresTop;
+        bool isPresRight;
+        bool isPresAngle;
+        bool isPresBottom;
+        int pWidth;
+        int pHeight;
+        Point pos = new Point();
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDown = true;
+            Control c = sender as Control;
+            pWidth = c.Width;
+            pHeight = c.Height;
+            pos.X = e.X;
+            pos.Y = e.Y;
+            /////////////////////////////////////
+            if ((e.X < c.Width && (c.Height - e.Y) < 20) && ((c.Width - e.X) < 20))
+            {
+                isPresAngle = true;
+            }
+            else
+            {
+                if ((c.Width - e.X) < 20)
+                {
+                    isPresRight = true;
+                }
+                else
+                {
+                    if (e.X < c.Width && (c.Height - e.Y) < 20)
+                    {
+                        isPresTop = true;
+                    }
+                    else
+                    {
+
+                        if (((c.Height + e.Y) - c.Height) < 25)
+                        {
+                            isPresBottom = true;
+                        }
+                    }
+                }
+            }
+            /////////////////////////////////
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Control c = sender as Control;
+            
+            if (isDown)
+            {
+                check(c, e);
+            }
+        }
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+                
+        {
+            isDown = false;
+            isPresTop = false;
+            isPresRight = false;
+            isPresAngle = false;
+            isPresBottom = false;
+        }
+
+        private void check(Control c, MouseEventArgs e)
+        {
+            Point newPos = new Point(e.X - pos.X, e.Y - pos.Y);
+            Point movePos = new Point(MousePosition.X - pos.X, MousePosition.Y - pos.Y);
+            
+            if ( isPresAngle)
+            {
+                c.Width = pWidth + newPos.X;
+                c.Height = pHeight + newPos.Y;
+            }
+            else
+            {
+                if ( isPresRight)
+                {
+                    c.Width = pWidth + newPos.X;
+                }
+                else
+                {
+                    if ( isPresTop)
+                    {
+                        c.Height = pHeight + newPos.Y;
+                    }
+                    else
+                    {
+
+                        if ( isPresBottom)
+                        {
+                            c.Location = PointToClient(movePos);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
